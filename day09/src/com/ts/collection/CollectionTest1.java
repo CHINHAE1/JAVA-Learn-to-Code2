67d52d034daa2bc290b4a9b0e3c642862ea71e14
CHINHAE1 <675351444@qq.com>
2024-05-29 17:49:22 +0800

    asd

--- day09/src/com/ts/collection/CollectionTest1.java
+++ day09/src/com/ts/collection/CollectionTest1.java
@@ -1,5 +1,7 @@
 package com.ts.collection;
 
+import com.ts.domain.Student;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -19,17 +21,27 @@ public class CollectionTest1 {
          public int size（）：返回集合中元素的个数（集合的长度）
   */
   public static void main(String[] args) {
+    Collection<Student> c = new ArrayList<>();
 
+    c.add(new Student("李四",17));
+    c.add(new Student("王五",11));
+    c.add(new Student("赵一",14));
+
+    c.remove(new Student("李四",17));
+    System.out.println(c);
+  }
+
+  private static void method() {
     //  以多态的形式创建集合对象,调用单列集合中的共有方法
     Collection<String> c = new ArrayList<>();
 
-    boolean a = c.add("A");
-    boolean b = c.add("A");
-    boolean d = c.add("A");
-    System.out.println(a);
-    System.out.println(b);
-    System.out.println(d);
+    c.add("李四");
+    c.add("王五");
+    c.add("赵一");
+    c.add("李四");
+    System.out.println(c);
 
+    c.remove("李四");
     System.out.println(c);
   }
 }
---
+++ day09/src/com/ts/domain/Student.java
@@ -0,0 +1,70 @@
+package com.ts.domain;
+
+
+import java.util.Objects;
+
+/**
+ * @Author CHINHAE @Date 2024/5/29 17:39 @PackageName:com.ts.domain @ClassName:
+ * Student @Description: TODO @Version 1.0
+ */
+public class Student {
+    private String name;
+    private int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return "Student{name = " + name + ", age = " + age + "}";
+    }
+}
